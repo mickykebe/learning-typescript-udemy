fee4ce6186616344cf3a13da1cf5a8e365b90483
mickykebe <mickykebe@gmail.com>
2020-02-08 12:03:33 +0300

    express login + decorators

---
+++ features/decorators.ts
@@ -0,0 +1,48 @@
+@classDecorator
+class Boat {
+  @testDecorator
+  color: string = "red";
+
+  @testDecorator
+  get formattedColor(): string {
+    return `This boat's color is ${this.color}`;
+  }
+
+  @logError("Oops boat was sunk in ocean")
+  pilot(
+    @paramDecorator speed: string,
+    @paramDecorator generateWake: boolean
+  ): void {
+    if (speed === "fast") {
+      console.log("swish");
+    } else {
+      console.log("nothing");
+    }
+  }
+}
+
+function classDecorator(constructor: typeof Boat) {
+  console.log(constructor);
+}
+
+function paramDecorator(target: any, key: string, index: number) {
+  console.log(key, index);
+}
+
+function testDecorator(target: any, key: string) {
+  console.log(key);
+}
+
+function logError(errorMessage: string) {
+  return function(target: any, key: string, desc: PropertyDescriptor): void {
+    const method = desc.value;
+
+    desc.value = function() {
+      try {
+        method();
+      } catch (e) {
+        console.log(errorMessage);
+      }
+    };
+  };
+}
