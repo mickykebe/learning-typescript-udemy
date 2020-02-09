e550fccc6272c7478f23a1d35f0381cb831a8524
mickykebe <mickykebe@gmail.com>
2020-02-09 12:02:25 +0300

    advanced express typescript integration

---
+++ server/build/AppRouter.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = require("express");
+var AppRouter = /** @class */ (function () {
+    function AppRouter() {
+    }
+    AppRouter.getInstance = function () {
+        if (!AppRouter.instance) {
+            AppRouter.instance = express_1.Router();
+        }
+        return AppRouter.instance;
+    };
+    return AppRouter;
+}());
+exports.AppRouter = AppRouter;
